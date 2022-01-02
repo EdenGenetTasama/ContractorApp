cdccc1b2793aedd8896c01f66062dbd0c7f10e0f
EdenGenetTasama <edentasama19@gmail.com>
2022-01-02 20:20:48 +0200

    Add project files.

---
+++ ContractorApp/Contractor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractorApp
+{
+    internal class Contractor { 
+        string name;
+    string dateOfBirth;
+    string email;
+    int payment;
+
+    public Contractor(string name, string dateOfBirth, string email, int payment)
+    {
+        this.Name = name;
+        this.DateOfBirth = dateOfBirth;
+        this.Email = email;
+        this.Payment = payment;
+    }
+
+    public string Name { get => name; set => name = value; }
+    public string DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+    public string Email { get => email; set => email = value; }
+    public int Payment { get => payment; set => payment = value; }
+}
+}
